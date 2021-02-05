b3904ff0c1bec44d3865c6224bcef59fda9c5b72
samjanisch <sjanisch77@gmail.com>
2021-02-05 10:36:58 -0800

    Add files via upload

---
+++ theDom.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('#container');
+
+const p = document.createElement('p');
+
+p.style.color = 'red';
+p.textContent = "Hey I'm red!";
+container.appendChild(p);
+
+const h3 = document.createElement('h3');
+
+h3.style.color = 'blue';
+h3.textContent = "I'm a blue h3!" 
+container.appendChild(h3);
+
+const div = document.createElement('div');
+div.classList.add('div');
+
+const h1 = document.createElement('h1');
+h1.textContent = "I'm in a div";
+div.appendChild(h1)
+
+const p2 = document.createElement('p');
+p2.textContent = "Me Too!"
+div.appendChild(p2)
+
+container.appendChild(div);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    alert("Hello World");
+})
